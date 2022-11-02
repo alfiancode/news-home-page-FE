a58412958873e0a4a2a7ac8e95ccd3ae7e4d4278
alfian <Rizal.yakul.2261@outlook.co.id>
2022-11-02 11:49:46 +0700

    Update Banner.jsx

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -7,20 +7,22 @@ const Banner = () => {
     <div>
       <div className="flex flex-col mx-5">
         <img src={imageWebMobile} alt="" />
-        <div className="flex flex-col">
+        <div className="flex flex-col md:flex-row space-x-3 mt-6 ">
           {" "}
-          <div className="font-black text-6xl mt-7 ">
+          <div className=" font-black text-6xl  md:basis-1/2 ">
             The Bright Future of Web 3.0?
           </div>
-          <p className="leading-relaxed tracking-wide mt-4 text-gray-700">
-            We dive into the next evolution of the web that claims to put the
-            power of the platforms back into the hands of the people. But is it
-            really fulfilling its promise?
-          </p>
-          {/* button read more */}
-          <div className="flex flex-row mt-5">
-            <div className="flex flex-row items-center justify-center bg-[#f15e50] rounded-md w-48 h-14">
-              <p className="text-white font-bold">Read More</p>
+          <div className="flex flex-col md:basis-1/2  justify-between">
+            <p className="leading-relaxed tracking-wide mt-7 md:mt-0  text-gray-700">
+              We dive into the next evolution of the web that claims to put the
+              power of the platforms back into the hands of the people. But is
+              it really fulfilling its promise?
+            </p>
+            {/* button read more */}
+            <div className="flex flex-row mt-5">
+              <div className="flex flex-row items-center justify-center bg-[#f15e50] rounded-md w-48 h-14">
+                <p className="text-white font-bold">Read More</p>
+              </div>
             </div>
           </div>
         </div>

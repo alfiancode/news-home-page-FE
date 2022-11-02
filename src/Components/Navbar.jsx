6da642715d7b2c3ce5654db2a19798165580ffa4
alfian <Rizal.yakul.2261@outlook.co.id>
2022-11-02 09:29:27 +0700

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
         <Transition
           enter="transform transition ease-in-out duration-500 sm:duration-700"
           enterFrom="translate-x-full"
-          enterTo="translate-x-0"
+          //   enterTo="translate-x-0"
           leave="transform transition ease-in-out duration-500 sm:duration-700"
           leaveFrom="translate-x-0"
           leaveTo="translate-x-full"
